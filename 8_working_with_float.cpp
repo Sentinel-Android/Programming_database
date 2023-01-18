ae2096730d908b8d596e4286c49848392992a4f3
Aditya Varma <adityavarma2106.pwap@gmail.com>
2023-01-18 16:30:48 +0530

    10 programs completed
    10 programs completed

---
+++ 8_working_with_float.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a=1.22f; //we may or may not use f at the end of the float value.
+    cout<<a;
+    return 0;
+}
---
+++ 9_working_with_sizeof.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+using namespace std;
+int main()
+{
+    double a=36436;
+    int size=sizeof(a);
+    cout<<size;
+    return 0;
+}
