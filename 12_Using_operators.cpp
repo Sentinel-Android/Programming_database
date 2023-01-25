b8e9cad7eccd68181290eb98aaba1519c7981f7d
Aditya Varma <adityavarma2106.pwap@gmail.com>
2023-01-25 12:04:21 +0530

    13 programs completed

---
+++ 12_Using_operators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<2/5<<endl;
+    /*int/int=int
+    float/int=float
+    int/float=float*/
+    int a=2.0/5; //floating point value is type casted to integer value
+    cout<<a;
+    return 0;
+}
---
+++ 13_Working_with_cin.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a=cin.get(); //returns the the value as ASCII values of the first character
+    cout<<a;
+    return 0;
+}
