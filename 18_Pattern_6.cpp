ddc606913d4d0294e12ce22b194f4c840ab6e7c7
Aditya Varma <adityavarma2106.pwap@gmail.com>
2023-01-25 16:01:28 +0530

    20 programs completed

--- 18_Pattern_6.cpp
+++ 18_Pattern_6.cpp
@@ -4,11 +4,9 @@ int main()
 {
     int n;
     cin>>n;
-    int count=1;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<count;
-            count++;
+        for(int j=1;j<=i;j++){
+            cout<<i;
         }
         cout<<endl;
     }
--- 19_Pattern_7.cpp
+++ 19_Pattern_7.cpp
@@ -6,7 +6,7 @@ int main()
     cin>>n;
     int count=1;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
+        for(int j=1;j<=i;j++){
             cout<<count;
             count++;
         }
